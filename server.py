73f03b00ead1527e6be3dc41049fdd1891fceec4
John Crispin <john@phrozen.org>
2020-10-31 12:44:26 +0100

    usync-py-ws: add a config file for the daemon
    Signed-off-by: John Crispin <john@phrozen.org>

--- server.py
+++ server.py
@@ -9,21 +9,24 @@ import ssl
 connected = set()
 clients = {}
 
+config = {}
+
 async def cmd_error(client, reply = {"error": "invalid command"}):
 	await client.send(json.dumps(reply))
 
-async def config_load_file(serial):
-	with open(f"configs/{serial}") as json_file:
+async def serial_load_file(serial):
+	with open(f"{config['config_folder']}{serial}") as json_file:
 		return json.load(json_file)
 
-async def config_load(client, cmd):
+async def serial_load(client, cmd):
 	serial = cmd["serial"]
 	uuid = cmd["uuid"]
 	try:
-		cfg = await config_load_file(serial)
+		cfg = await serial_load_file(serial)
 		clients[serial] = {"client": client, "serial": serial, "uuid": uuid, "config": cfg,}
 		print(f"{serial} config loaded")
 	except:
+		print(f"{serial} no config found")
 		await cmd_error(client, {"error": "unknown serial" })
 
 async def state_store(serial, state):
@@ -49,7 +52,7 @@ async def server(client, path):
 				serial = cmd["serial"]
 				uuid = cmd["uuid"]
 				if serial not in clients:
-					await config_load(client, cmd)
+					await serial_load(client, cmd)
 				elif clients[serial]["uuid"] != uuid:
 					clients[serial]["uuid"] = uuid
 					print(f"{serial} received new config")
@@ -73,7 +76,7 @@ async def timer():
 				del clients[serial]
 				continue
 			try:
-				client["config"] = await config_load_file(serial)
+				client["config"] = await serial_load_file(serial)
 			except:
 				print(f"{serial} config failed to load")
 				continue;
@@ -82,10 +85,17 @@ async def timer():
 				await client["client"].send(json.dumps({"cfg": client["config"]}))
 		await asyncio.sleep(5)
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain("cert.pem", "key.pem")
+with open(f"usync.cfg") as json_file:
+	config = json.load(json_file)
+for k in { "ssl_key", "ssl_cert", "bind", "port", "config_folder" }:
+	if k not in config:
+		print(f"{k} is missing from in the config file")
+		exit(-1)
 
-start_server = websockets.serve(server, "localhost", 8765,
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(config["ssl_cert"], config["ssl_key"])
+
+start_server = websockets.serve(server, config["bind"], config["port"],
 	ssl=ssl_context,
 	create_protocol=websockets.basic_auth_protocol_factory(
 		realm="venue", credentials=("test", "test")
